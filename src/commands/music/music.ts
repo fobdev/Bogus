9aa0054512bf4e89dbcb0869e852c6bbec69d660
fobdev <silvapedro55@gmail.com>
2021-11-02 18:02:21 -0300

    first implementation

---
+++ src/commands/music/music.ts
@@ -0,0 +1,8 @@
+import { Command } from "../../interfaces";
+import { Response } from "../../models";
+
+export const Music: Command = {
+    name: "music",
+    description: "Discord Bot Music",
+    run: async (client, message) => {},
+};
