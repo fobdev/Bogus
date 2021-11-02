e7c05976adea848eb234d594184593e9d7465c73
fobdev <silvapedro55@gmail.com>
2021-11-02 20:01:03 -0300

    first implementation of help

--- src/commands/bot/help.ts
+++ src/commands/bot/help.ts
@@ -1,7 +1,17 @@
 import { Command } from "../../interfaces";
+import { CommandList } from "../_CommandList";
 
 export const Help: Command = {
     name: ["help"],
     description: "Display all the available commands.",
-    run: async (client, message) => {},
+    run: async (client, message, args) => {
+        let { channel } = message;
+
+        if (args?.length! <= 0)
+            return channel.send("Please input a command from which you need help.");
+
+        for (const Command of CommandList)
+            if (Command.name.find((element) => element === args![0]))
+                return channel.send(Command.description);
+    },
 };
