cb6242198ec265d0e892315cee68635514e35ed7
PedroDev <silvapedro55@gmail.com>
2021-11-02 15:54:30 -0300

    changed project files from old one

---
+++ README.md
@@ -0,0 +1,9 @@
+# Bot2 Client (noname)
+
+### Instructions
+
+-   `git clone https://github.com/fobdev/bot2.git`
+-   `cd bot2`
+-   Add your bot token to the `.env` file
+-   `npm run build`
+-   `npm start`
--- src/commands/_CommandList.ts
+++ src/commands/_CommandList.ts
@@ -6,7 +6,6 @@ import { Clear } from "./admin/clear";
 import { Kick } from "./admin/kick";
 import { RenameServer } from "./admin/renameserver";
 import { Ban } from "./admin/ban";
-import { Music } from "./music/music";
 
 export const CommandList: CommandInt[] = [
     HelloWorld,
@@ -16,5 +15,4 @@ export const CommandList: CommandInt[] = [
     Kick,
     RenameServer,
     Ban,
-    Music,
 ];
