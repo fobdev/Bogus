5b00a1e7346194ef75693621e619a42ccf698c39
fobdev <silvapedro55@gmail.com>
2021-11-02 19:13:57 -0300

    added name array support

--- src/events/onMessage.ts
+++ src/events/onMessage.ts
@@ -3,14 +3,23 @@ import { CommandList } from "../commands/_CommandList";
 
 export const onMessage = async (client: Client, message: Message) => {
     const prefix = ">";
-    const args = message.content.split(" ").slice(1);
+
+    let { content } = message;
+
+    const args = content.split(" ").slice(1);
 
     if (!message.content.startsWith(prefix)) return;
     if (message.author.bot) return;
 
-    for (const Command of CommandList)
-        if (message.content.startsWith(Command.name, prefix.length))
-            await Command.run(client, message, args);
+    for (const Command of CommandList) {
+        Command.name.forEach(async (element) => {
+            const filterSingle = Command.name.filter(
+                (val) => val === content.substring(prefix.length)
+            )[0];
+
+            if (element === filterSingle) return await Command.run(client, message, args);
+        });
+    }
 
     return console.log(`${message.author.tag} just said [${message.content}]`);
 };
