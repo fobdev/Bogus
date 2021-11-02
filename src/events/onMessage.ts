5ca7860c694453fe64b2d42d6da0b3e02fc17c29
fobdev <silvapedro55@gmail.com>
2021-11-02 19:23:32 -0300

    removed debug logs

--- src/events/onMessage.ts
+++ src/events/onMessage.ts
@@ -7,10 +7,8 @@ export const onMessage = async (client: Client, message: Message) => {
     if (!message.content.startsWith(prefix)) return;
     if (message.author.bot) return;
 
-    console.log("entered1");
     for (const Command of CommandList) {
         if (message.content.startsWith(Command.name, prefix.length)) {
-            console.log("entered2");
             await Command.run(client, message);
         }
     }
