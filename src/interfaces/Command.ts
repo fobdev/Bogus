32a1acd4375a2004dae0927e02ce2cb2a0168c28
fobdev <silvapedro55@gmail.com>
2021-11-02 18:31:44 -0300

    added optional args param

--- src/interfaces/Command.ts
+++ src/interfaces/Command.ts
@@ -1,7 +1,6 @@
 import { Client, Message } from "discord.js";
-
 export interface Command {
     name: string;
     description: string;
-    run: (client: Client, message: Message) => any;
+    run: (client: Client, message: Message, args?: Array<string>) => any;
 }
