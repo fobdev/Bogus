fdd21068d0a9a3fe3097bcbe91c7ba70fb307dde
fobdev <silvapedro55@gmail.com>
2021-11-02 18:38:25 -0300

    added args to received message

--- src/events/onMessage.ts
+++ src/events/onMessage.ts
@@ -3,15 +3,14 @@ import { CommandList } from "../commands/_CommandList";
 
 export const onMessage = async (client: Client, message: Message) => {
     const prefix = ">";
+    const args = message.content.split(" ").slice(1);
 
     if (!message.content.startsWith(prefix)) return;
     if (message.author.bot) return;
 
-    for (const Command of CommandList) {
-        if (message.content.startsWith(Command.name, prefix.length)) {
-            await Command.run(client, message);
-        }
-    }
+    for (const Command of CommandList)
+        if (message.content.startsWith(Command.name, prefix.length))
+            await Command.run(client, message, args);
 
     return console.log(`${message.author.tag} just said [${message.content}]`);
 };
