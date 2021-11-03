630a230f1d7e5bb27055a5886fdfe0e1a71fc3de
fobdev <silvapedro55@gmail.com>
2021-11-03 01:01:10 -0300

    added new optional arguments param for documentation porpuses

--- src/interfaces/Command.ts
+++ src/interfaces/Command.ts
@@ -1,6 +1,7 @@
 import { Client, Message } from "discord.js";
 export interface Command {
     name: Array<string>;
-    description: string;
+    arguments?: Array<string>; // Optional for documentation porpuses
+    description: string; // Necessary for documentation porpuses
     run: (client: Client, message: Message, args?: Array<string>) => any;
 }
