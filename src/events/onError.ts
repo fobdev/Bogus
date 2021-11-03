3f29af374715f8bb59618620155928ea55f966eb
fobdev <silvapedro55@gmail.com>
2021-11-03 12:28:48 -0300

    better error output

--- src/events/onError.ts
+++ src/events/onError.ts
@@ -4,7 +4,11 @@ import { Response } from "../models";
 export const onError = (message: Message, error: any) => {
     return message.channel.send({
         embeds: [
-            Response(`Error: ${error.message}`, "The bot was unable to run this command.", "FAIL"),
+            Response(
+                `Error: ${error.message}`,
+                "The bot was unable to run this command properly.",
+                "FAIL"
+            ),
         ],
     });
 };
