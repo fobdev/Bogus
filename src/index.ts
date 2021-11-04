000632801e92197055e53fc896fd158cbeef86ac
fobdev <silvapedro55@gmail.com>
2021-11-04 14:30:39 -0300

    worked intents to player / set player

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,26 @@
 import { Client, Intents } from "discord.js";
 import { onMessage, onReady } from "./events";
+import { Player } from "discord-player";
+import { onPlayer } from "./events/onPlayer";
 
 (async () => {
     const client = new Client({
-        intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
+        intents: [
+            Intents.FLAGS.GUILDS,
+            Intents.FLAGS.GUILD_MESSAGES,
+            Intents.FLAGS.GUILD_VOICE_STATES,
+        ],
     });
+
+    const player = new Player(client);
+
+    onPlayer(player);
 
     // calls when the client starts
     client.on("ready", async () => await onReady());
 
     // calls when a new message is sent in any channel
-    client.on("messageCreate", async (message) => await onMessage(client, message));
+    client.on("messageCreate", async (message) => await onMessage(client, player, message));
 
     await client.login(process.env.BOT_TOKEN);
 })();
