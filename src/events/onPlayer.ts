c6996faebb8ea1019f1ae49ef7d07248cdbd8185
fobdev <silvapedro55@gmail.com>
2021-11-04 16:58:15 -0300

    better tarckstart logic

--- src/events/onPlayer.ts
+++ src/events/onPlayer.ts
@@ -3,6 +3,43 @@ import { Channel, Emoji, Message, MessageActionRow, MessageButton } from "discor
 import { Response } from "../models";
 
 export const onPlayer = async (player: Player) => {
+    let editableResponse = player.on("trackStart", (queue, track) => {
+        // @ts-ignore
+        return queue.metadata?.channel.send({
+            embeds: [
+                Response(
+                    `:musical_note: Now Playing: **${track.title}**`,
+                    `Duration: ${track.duration}\nAuthor: ${track.author}\nAdded by: ${track.requestedBy}`,
+                    "OTHER",
+                    "PURPLE"
+                )
+                    .setThumbnail(track.thumbnail)
+                    .setURL(track.url)
+                    .setFooter(
+                        `Next track: ${
+                            queue.tracks.length > 0
+                                ? `${queue.tracks[0].title} by ${queue.tracks[0].author}`
+                                : "none"
+                        }`
+                    ),
+            ],
+            components: [
+                new MessageActionRow().addComponents(
+                    new MessageButton()
+                        .setCustomId("button-stop")
+                        .setLabel("STOP")
+                        .setStyle("SECONDARY")
+                        .setEmoji("🛑"),
+                    new MessageButton()
+                        .setCustomId("button-skip")
+                        .setLabel("SKIP TRACK")
+                        .setStyle("SECONDARY")
+                        .setEmoji("➡️")
+                ),
+            ],
+        });
+    });
+
     player.on("trackAdd", (queue: Queue, track: Track) => {
         // @ts-ignore
         return queue.metadata?.channel.send(`Added to the queue: **${track}** (${track.duration})`);
@@ -11,6 +48,7 @@ export const onPlayer = async (player: Player) => {
     player.on("error", (queue: Queue, error) => {
         console.log(error);
 
+        if (error.name === "DestroyedQueue") return;
         // @ts-ignore
         return queue.metadata?.channel.send({
             embeds: [
