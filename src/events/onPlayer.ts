6eb47618d489da827f19de78caec0fe00c3ff3a4
fobdev <silvapedro55@gmail.com>
2021-11-04 23:13:38 -0300

    changes to track adding

--- src/events/onPlayer.ts
+++ src/events/onPlayer.ts
@@ -1,18 +1,25 @@
 import { Player, Queue, Track } from "discord-player";
 import { Channel, Emoji, Message, MessageActionRow, MessageButton } from "discord.js";
 import { Response } from "../models";
+import botconfig from "../botconfig.json";
 
 export const onPlayer = async (player: Player) => {
-    let editableResponse = player.on("trackStart", (queue, track) => {
+    player.on("trackStart", async (queue, track) => {
         // @ts-ignore
-        return queue.metadata?.channel.send({
+        return await queue.metadata?.channel.send({
             embeds: [
                 Response(
                     `:musical_note: Now Playing: **${track.title}**`,
-                    `Duration: ${track.duration}\nAuthor: ${track.author}\nAdded by: ${track.requestedBy}`,
+
+                    // @ts-ignore
+                    `Requested in ${queue.metadata?.channel}.`,
                     "OTHER",
                     "PURPLE"
                 )
+                    .addField("Author:", track.author)
+                    .addField("Duration:", track.duration, true)
+                    .addField("Added by:", `${track.requestedBy}`, true)
+
                     .setThumbnail(track.thumbnail)
                     .setURL(track.url)
                     .setFooter(
@@ -26,17 +33,42 @@ export const onPlayer = async (player: Player) => {
         });
     });
 
-    player.on("trackAdd", (queue: Queue, track: Track) => {
+    player.on("trackAdd", async (queue: Queue, track: Track) => {
         // @ts-ignore
-        return queue.metadata?.channel.send(`Added to the queue: **${track}** (${track.duration})`);
+        return await queue.metadata?.channel.send({
+            embeds: [
+                Response(
+                    `${track.title} added to the queue.`,
+                    `Added by: ${track.requestedBy}`,
+                    "OTHER",
+                    "PURPLE"
+                )
+                    .setThumbnail(track.thumbnail)
+                    .addField("Duration:", track.duration),
+            ],
+        });
     });
 
-    player.on("error", (queue: Queue, error) => {
-        console.log(error);
-
-        if (error.name === "DestroyedQueue") return;
+    player.on("tracksAdd", async (queue, tracks) => {
         // @ts-ignore
-        return queue.metadata?.channel.send({
+        return await queue.metadata?.channel.send({
+            embeds: [
+                Response(
+                    `${tracks.length} tracks added to the queue.`,
+                    "Use ``" + `${botconfig.prefix}queue` + "`` to see the result.",
+                    "OTHER",
+                    "PURPLE"
+                )
+                    .addField("Added by:", `${tracks[0].requestedBy}`)
+                    .setThumbnail(tracks[0].thumbnail),
+            ],
+        });
+    });
+
+    player.on("error", async (queue: Queue, error) => {
+        console.log(error);
+        // @ts-ignore
+        return await queue.metadata?.channel.send({
             embeds: [
                 Response(
                     "Error 403",
@@ -47,16 +79,16 @@ export const onPlayer = async (player: Player) => {
         });
     });
 
-    player.on("channelEmpty", (queue: Queue) => {
+    player.on("channelEmpty", async (queue: Queue) => {
         // @ts-ignore
-        return queue.metadata?.channel.send({
+        return await queue.metadata?.channel.send({
             embeds: [Response("No one in the voice channel.", "I'm leaving now.", "WARN")],
         });
     });
 
-    player.on("queueEnd", (queue: Queue) => {
+    player.on("queueEnd", async (queue: Queue) => {
         // @ts-ignore
-        return queue.metadata?.channel.send({
+        return await queue.metadata?.channel.send({
             embeds: [Response("I'm done.", "Leaving voice channel", "SUCCESS")],
         });
     });
