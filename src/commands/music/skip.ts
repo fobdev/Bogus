eac3fcd9e8dbda0e18fbecda8046010fd9dce092
fobdev <silvapedro55@gmail.com>
2021-11-04 21:15:38 -0300

    new skip command

---
+++ src/commands/music/skip.ts
@@ -0,0 +1,38 @@
+import { Command } from "../../interfaces";
+import { Response } from "../../models";
+
+export const SkipCommand: Command = {
+    name: ["skip", "s"],
+    description: "Skip the current track.",
+    run: async (client, message, args, player) => {
+        const { guild, channel } = message;
+
+        const skippingQueue = player?.getQueue(guild!.id);
+        if (!skippingQueue || !skippingQueue.playing)
+            return channel.send({
+                embeds: [
+                    Response(
+                        "Nothing is being played.",
+                        "You need to play something first.",
+                        "WARN"
+                    ),
+                ],
+            });
+
+        let success;
+        success = skippingQueue!.skip();
+        if (success && skippingQueue.tracks.length > 0)
+            return channel.send({
+                embeds: [
+                    Response(
+                        success ? "Track Skipped" : "Error Skipping Track",
+                        success
+                            ? `Now loading: **${skippingQueue.tracks[0].title}**...`
+                            : "Error trying to skip track",
+                        success ? "SUCCESS" : "FAIL"
+                    ),
+                ],
+            });
+        else return;
+    },
+};
