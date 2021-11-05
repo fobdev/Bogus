cf16c77362f229cc90afa6579b09226e84a8a726
fobdev <silvapedro55@gmail.com>
2021-11-04 23:12:03 -0300

    optimized exports

--- src/commands/music/index.ts
+++ src/commands/music/index.ts
@@ -2,3 +2,6 @@ export * from "./play";
 export * from "./skip";
 export * from "./leave";
 export * from "./queue";
+export * from "./shuffle";
+export * from "./nowplaying";
+export * from "./jump";
