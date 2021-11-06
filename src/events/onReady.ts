edd0a8d3d75f563654b6097613e0fe9514bcc606
fobdev <silvapedro55@gmail.com>
2021-11-05 21:00:41 -0300

    now activity display guilds

--- src/events/onReady.ts
+++ src/events/onReady.ts
@@ -1,7 +1,12 @@
 import { Client } from "discord.js";
+import botconfig from "../botconfig.json";
 
 export const onReady = async (client: Client) => {
-    client.user?.setActivity(">help", { type: "LISTENING" });
+    const guilds = client.guilds.cache;
+    client.user?.setActivity(`${botconfig.prefix}help @ ${guilds.size} servers`, {
+        type: "LISTENING",
+    });
+
     console.log(`===== Client connected as [${client.user?.username}] =====`);
-    console.table(client.guilds.cache.map((guild) => guild.name));
+    console.table(guilds.map((guild) => guild.name));
 };
