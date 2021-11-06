4a8963be3054ed2b12edbe0a904cc6d54495d358
fobdev <silvapedro55@gmail.com>
2021-11-06 10:31:36 -0300

    lytics bigger than message limit now are displayable

--- src/commands/music/lyrics.ts
+++ src/commands/music/lyrics.ts
@@ -1,6 +1,8 @@
 import { Command } from "../../interfaces";
 import { Response } from "../../models";
 import { Lyrics } from "@discord-player/extractor";
+import { Util } from "discord.js";
+import { Queue } from "../../../node_modules/discord-player/dist";
 import botconfig from "../../botconfig.json";
 
 export const Lyric: Command = {
@@ -12,26 +14,56 @@ export const Lyric: Command = {
         const lyricsClient = Lyrics.init();
         const singingQueue = player?.getQueue(guild!.id);
 
+        const largeLyrics = (search: Lyrics.LyricsData, queue?: Queue) => {
+            channel.send({
+                embeds: [
+                    Response(
+                        `Lyrics of ${queue ? queue.current.title : search.title}`,
+                        "The lyrics will be sent below because they're too big.",
+                        "OTHER",
+                        "PURPLE"
+                    )
+                        .setFooter(
+                            `If this was not what you're looking for, try using ${botconfig.prefix}lyrics [search]`
+                        )
+                        .setThumbnail(search.thumbnail),
+                ],
+            });
+
+            Util.splitMessage(search.lyrics!).forEach((chunk) => {
+                return channel.send(chunk);
+            });
+        };
+
         if (args![0]) {
             lyricsClient
                 .search(`${args!.join(" ")}`)
                 .then((search) => {
+                    if (search.lyrics!.length < 4096)
+                        return channel.send({
+                            embeds: [
+                                Response(
+                                    `Lyrics of ${search.title}`,
+                                    `${search.lyrics}`,
+                                    "OTHER",
+                                    "PURPLE"
+                                ).setThumbnail(search.thumbnail),
+                            ],
+                        });
+
+                    largeLyrics(search);
+                })
+                .catch((e: any) => {
+                    console.error("No lyrics found: ", e.message);
                     return channel.send({
                         embeds: [
                             Response(
-                                `Lyrics of ${search.title}`,
-                                `${search.lyrics}`,
-                                "OTHER",
-                                "PURPLE"
-                            ).setThumbnail(search.thumbnail),
-                        ],
-                    });
-                })
-                .catch((e: any) => {
-                    console.error(e.message);
-                    return channel.send({
-                        embeds: [
-                            Response(`Error`, `You request could not find any lyrics.`, "WARN"),
+                                `Error`,
+                                `You request could not find any lyrics.`,
+                                "WARN"
+                            ).setFooter(
+                                `You can also use ${botconfig.prefix}lyrics [search] to search for a specific song.`
+                            ),
                         ],
                     });
                 });
@@ -44,26 +76,35 @@ export const Lyric: Command = {
             lyricsClient
                 .search(`${singingQueue.current.title} ${singingQueue.current.author}`)
                 .then((search) => {
+                    if (search.lyrics!.length < 4096)
+                        return channel.send({
+                            embeds: [
+                                Response(
+                                    `Lyrics of ${singingQueue.current.title}`,
+                                    `${search.lyrics}`,
+                                    "OTHER",
+                                    "PURPLE"
+                                )
+                                    .setThumbnail(search.thumbnail)
+                                    .setFooter(
+                                        `If this was not what you're looking for, try using ${botconfig.prefix}lyrics [search]`
+                                    ),
+                            ],
+                        });
+
+                    largeLyrics(search, singingQueue);
+                })
+                .catch((e: any) => {
+                    console.error("No lyrics found: ", e.message);
                     return channel.send({
                         embeds: [
                             Response(
-                                `Lyrics of ${singingQueue.current.title}`,
-                                `${search.lyrics}`,
-                                "OTHER",
-                                "PURPLE"
-                            )
-                                .setThumbnail(search.thumbnail)
-                                .setFooter(
-                                    `If this was not what you're looking for, try using ${botconfig.prefix}lyrics [search]`
-                                ),
-                        ],
-                    });
-                })
-                .catch((e: any) => {
-                    console.error(e.message);
-                    return channel.send({
-                        embeds: [
-                            Response(`Error`, `You request could not find any lyrics.`, "WARN"),
+                                `Error`,
+                                `You request could not find any lyrics.`,
+                                "WARN"
+                            ).setFooter(
+                                `You can also use ${botconfig.prefix}lyrics [search] to search for a specific song.`
+                            ),
                         ],
                     });
                 });
