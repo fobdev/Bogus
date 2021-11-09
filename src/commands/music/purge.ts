aee59cd1577308b23264cc2ed7a91a4bad172ec0
fobdev <silvapedro55@gmail.com>
2021-11-09 15:08:44 -0300

    purge command added

---
+++ src/commands/music/purge.ts
@@ -0,0 +1,34 @@
+import { Command } from "../../interfaces";
+import { Response } from "../../models";
+
+export const Purge: Command = {
+    name: ["purge"],
+    description: "Purges the current queue.",
+    run: async (client, message, args, player) => {
+        const { guild, channel } = message;
+        const purgingQueue = player?.getQueue(guild!.id);
+
+        if (!purgingQueue || !purgingQueue.playing)
+            return channel.send({
+                embeds: [
+                    Response(
+                        "Nothing is playing right now.",
+                        "Please play something before using this command",
+                        "WARN"
+                    ),
+                ],
+            });
+
+        purgingQueue.clear();
+
+        return channel.send({
+            embeds: [
+                Response(
+                    "Queue purged.",
+                    "All the songs in the server queue have been removed.",
+                    "WARN"
+                ),
+            ],
+        });
+    },
+};
