4b6bb5013593199e5daa3610f75ad99382b04ba2
fobdev <silvapedro55@gmail.com>
2021-11-17 18:32:20 -0300

    only admins can change prefix now

--- src/commands/bot/prefix.ts
+++ src/commands/bot/prefix.ts
@@ -1,3 +1,4 @@
+import { Permissions } from "discord.js";
 import { setPrefix } from "../../db";
 import { Command } from "../../interfaces";
 import { Response } from "../../models";
@@ -6,7 +7,19 @@ export const Prefix: Command = {
     name: ["prefix"],
     description: "Changes the bot prefix for this server.",
     run: async (prefix, client, message, args, player, postgres) => {
-        const { channel, guild } = message;
+        const { member, channel, guild } = message;
+
+        if (!member?.permissions.has(Permissions.FLAGS.ADMINISTRATOR)) {
+            return channel.send({
+                embeds: [
+                    Response(
+                        "Permission denied.",
+                        "Only admins can change the bot prefix of the server.",
+                        "FAIL"
+                    ),
+                ],
+            });
+        }
 
         if (!args![0] || args!.length > 1 || args![0].length > 3)
             return channel.send({
