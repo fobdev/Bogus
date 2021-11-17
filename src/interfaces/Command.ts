03ce98db593c5639c6586069e3b174f0b047a58a
fobdev <silvapedro55@gmail.com>
2021-11-17 18:13:20 -0300

    now prefix is a param

--- src/interfaces/Command.ts
+++ src/interfaces/Command.ts
@@ -1,8 +1,16 @@
+import { PoolClient as PostgresClient } from "pg";
 import { Player } from "discord-player";
 import { Client, Message } from "discord.js";
 export interface Command {
     name: Array<string>;
     arguments?: Array<string>; // Optional for documentation porpuses
     description: string; // Necessary for documentation porpuses
-    run: (client: Client, message: Message, args?: Array<string>, player?: Player) => any;
+    run: (
+        prefix: string,
+        client: Client,
+        message: Message,
+        args?: Array<string>,
+        player?: Player,
+        postgres?: PostgresClient
+    ) => any;
 }
