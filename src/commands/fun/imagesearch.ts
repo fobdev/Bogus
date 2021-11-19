9dff5bbb8a150cd2fe24d112ae51d4c1f05e3ba9
fobdev <silvapedro55@gmail.com>
2021-11-19 15:23:43 -0300

    image search command created

---
+++ src/commands/fun/imagesearch.ts
@@ -0,0 +1,91 @@
+import { Command } from "../../interfaces";
+import GoogleImages from "google-images";
+import { MessageActionRow, MessageButton, MessageOptions } from "discord.js";
+import { Response } from "../../models";
+import ms from "ms";
+
+export const ImageSearch: Command = {
+    name: ["image"],
+    description: "Search for images using the Google API",
+    run: async (prefix, client, message, args) => {
+        const { channel } = message;
+        const searchClient = new GoogleImages(
+            process.env.GOOGLE_CUSTOM_SEARCH_ENGINE_ID!,
+            process.env.GOOGLE_API_KEY!
+        );
+
+        if (args?.length === 0)
+            return channel.send({
+                embeds: [Response("Error", "You need to search for something", "FAIL")],
+            });
+
+        const prevButton = new MessageButton()
+            .setCustomId(Math.random().toString())
+            .setLabel("Previous")
+            .setStyle("SECONDARY");
+
+        const nextButton = new MessageButton()
+            .setCustomId(Math.random().toString())
+            .setLabel("Next")
+            .setStyle("SECONDARY");
+
+        await searchClient.search(args!.join(" ")).then((images) => {
+            let iterator = 0;
+            const sendObject: MessageOptions = {
+                components: [new MessageActionRow().addComponents(prevButton, nextButton)],
+                embeds: [
+                    Response(
+                        `Results for ${args?.join(" ")} ( ${iterator + 1} / ${images.length} )`,
+                        "",
+                        "SUCCESS"
+                    ).setImage(images[iterator].url),
+                ],
+            };
+
+            return channel.send(sendObject).then((msg) => {
+                const buttonsCollector = msg.channel.createMessageComponentCollector({
+                    filter: (i) =>
+                        (i.customId === prevButton.customId ||
+                            i.customId === nextButton.customId) &&
+                        i.user.id === message.author.id,
+                    idle: ms("30s"),
+                });
+
+                buttonsCollector.on("end", () => {
+                    sendObject.components = [];
+                    msg.edit(sendObject);
+                });
+
+                buttonsCollector.on("collect", (interaction) => {
+                    interaction.deferUpdate();
+
+                    switch (interaction.customId) {
+                        case prevButton.customId:
+                            iterator--;
+                            if (iterator < 0) iterator = images.length - 1;
+                            break;
+                        case nextButton.customId:
+                            iterator++;
+                            if (iterator > images.length - 1) iterator = 0;
+                            break;
+                        default:
+                            return;
+                    }
+
+                    msg.edit({
+                        components: [new MessageActionRow().addComponents(prevButton, nextButton)],
+                        embeds: [
+                            Response(
+                                `Results for ${args?.join(" ")} ( ${iterator + 1} / ${
+                                    images.length
+                                } )`,
+                                "",
+                                "SUCCESS"
+                            ).setImage(images[iterator].url),
+                        ],
+                    });
+                });
+            });
+        });
+    },
+};
