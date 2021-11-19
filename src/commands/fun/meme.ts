f4fe0f27514dfde083b2aec4af970f32bb85964f
fobdev <silvapedro55@gmail.com>
2021-11-19 11:11:50 -0300

    meme command created

---
+++ src/commands/fun/meme.ts
@@ -0,0 +1,74 @@
+import Canvas, { loadImage, createCanvas } from "canvas";
+import { MessageAttachment } from "discord.js";
+import { Response } from "../../models";
+import { Command } from "../../interfaces";
+
+export const Meme: Command = {
+    name: ["meme"],
+    description: "Make any image a meme.",
+    run: async (prefix, client, message, args) => {
+        const { channel } = message;
+        let attachment;
+
+        const input = args!.join(" ");
+
+        try {
+            attachment = await (await message.fetchReference()).attachments;
+            if (attachment.size === 0) throw new Error();
+        } catch (error) {
+            return channel.send({
+                embeds: [Response("Error", "You need to reply to an image.", "FAIL")],
+            });
+        }
+
+        const width = attachment.map((element) => element.width)[0]!;
+        const height = attachment.map((element) => element.height)[0]!;
+
+        const canvas = createCanvas(width, height);
+        const ctx = canvas.getContext("2d");
+        const image = await Canvas.loadImage(attachment.map((element) => element.url)[0]);
+
+        ctx.drawImage(image, 0, 0, width, height);
+
+        ctx.font = "bold 200px Impact";
+        ctx.textAlign = "center";
+
+        // Top Text
+        ctx.textBaseline = "top";
+        ctx.fillStyle = "white";
+        ctx.fillText(
+            input.includes("/") ? input.split("/")[0].trimEnd() : input,
+            width / 2,
+            0,
+            width
+        );
+        ctx.fillStyle = "black";
+        ctx.strokeText(
+            input.includes("/") ? input.split("/")[0].trimEnd() : input,
+            width / 2,
+            0,
+            width
+        );
+
+        // Bottom Text
+        ctx.textBaseline = "bottom";
+        ctx.fillStyle = "white";
+        ctx.fillText(
+            input.includes("/") ? input.split("/")[1].trimStart() : "",
+            width / 2,
+            height,
+            width
+        );
+        ctx.fillStyle = "black";
+        ctx.strokeText(
+            input.includes("/") ? input.split("/")[1].trimStart() : "",
+            width / 2,
+            height,
+            width
+        );
+
+        return channel.send({
+            files: [new MessageAttachment(canvas.toBuffer())],
+        });
+    },
+};
