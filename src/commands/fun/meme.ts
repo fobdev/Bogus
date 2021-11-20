32d024e6af6090d1c06eaa131acfaee7532f5ed2
fobdev <silvapedro55@gmail.com>
2021-11-20 14:25:57 -0300

    now the processing is made by a standalone server

--- src/commands/fun/meme.ts
+++ src/commands/fun/meme.ts
@@ -1,7 +1,7 @@
-import Canvas, { createCanvas } from "canvas";
-import { Collection, MessageAttachment, MessageEmbedImage } from "discord.js";
+import { BufferResolvable, Collection, MessageAttachment, MessageEmbedImage } from "discord.js";
 import { Response } from "../../models";
 import { Command } from "../../interfaces";
+import axios from "axios";
 
 export const Meme: Command = {
     name: ["meme"],
@@ -36,65 +36,34 @@ export const Meme: Command = {
             }
         }
 
-        const canvas = createCanvas(width!, height!);
-        const ctx = canvas.getContext("2d");
+        // Gets from the image processing server
+        const api = axios.create({ baseURL: "https://bogus-image-processing.herokuapp.com/" });
 
-        let image;
-        try {
-            image = await Canvas.loadImage(imageURL!);
-        } catch (error) {
-            return channel.send({
-                embeds: [Response("Error", "The file type is not supported.", "FAIL")],
-            });
+        interface Content {
+            type: string;
+            data: Array<number>;
+        }
+        interface ResponseSchema {
+            image: Content;
         }
 
-        const japanese: RegExp =
-            /[\u3000-\u303f\u3040-\u309f\u30a0-\u30ff\uff00-\uff9f\u4e00-\u9faf\u3400-\u4dbf]/;
+        // input setting
+        let topText = input.includes("/") ? input.split("/")[0].trimEnd() : input;
+        let bottomText = input.includes("/") ? input.split("/")[1].trimStart() : " ";
 
-        ctx.drawImage(image, 0, 0, width, height);
-
-        Canvas.registerFont("./src/fonts/Impact.ttf", { family: "Sans-Serif" });
-        Canvas.registerFont("./src/fonts/NotoSansJP-Bold.otf", { family: "Sans-Serif" });
-
-        ctx.font = `bold ${width! / 10}px ${input.match(japanese) ? "NotoSansJP-Bold" : "Impact"}`;
-        ctx.textAlign = "center";
-
-        // Top Text
-        ctx.textBaseline = "top";
-        ctx.fillStyle = "white";
-        ctx.fillText(
-            input.includes("/") ? input.split("/")[0].trimEnd() : input,
-            width! / 2,
-            0,
-            width
-        );
-        ctx.fillStyle = "black";
-        ctx.strokeText(
-            input.includes("/") ? input.split("/")[0].trimEnd() : input,
-            width! / 2,
-            0,
-            width
-        );
-
-        // Bottom Text
-        ctx.textBaseline = "bottom";
-        ctx.fillStyle = "white";
-        ctx.fillText(
-            input.includes("/") ? input.split("/")[1].trimStart() : "",
-            width! / 2,
-            height!,
-            width
-        );
-        ctx.fillStyle = "black";
-        ctx.strokeText(
-            input.includes("/") ? input.split("/")[1].trimStart() : "",
-            width! / 2,
-            height!,
-            width
-        );
-
-        return channel.send({
-            files: [new MessageAttachment(canvas.toBuffer())],
-        });
+        try {
+            await api
+                .get<ResponseSchema>(`/${topText ? topText : " "}/${bottomText}/${imageURL}`, {
+                    responseType: "json",
+                })
+                .then((response) => {
+                    return channel.send({
+                        files: [new MessageAttachment(Buffer.from(response.data.image.data))],
+                    });
+                });
+        } catch (error) {
+            console.error(error);
+            return channel.send("Error with GET request from bogue-image-processing server.");
+        }
     },
 };
