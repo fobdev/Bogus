d0eb4d0132f253efb1a14565b389092cb8550c12
fobdev <silvapedro55@gmail.com>
2021-11-22 14:41:42 -0300

    bug reporting command added

---
+++ src/commands/admin/bug.ts
@@ -0,0 +1,29 @@
+import { Command } from "../../interfaces";
+import { Response } from "../../models";
+
+export const Bug: Command = {
+    name: ["bug"],
+    arguments: ["description"],
+    description: "Report bugs to the developer.",
+    run: async (prefix, client, message, args) => {
+        const { author, channel, guild } = message;
+
+        if (args?.length === 0)
+            return channel.send({
+                embeds: [Response("Error", "You need to describe the bug.", "FAIL")],
+            });
+
+        const dev = client.users.cache.find((user) => user.id === process.env.DEVELOPER_ID);
+        if (!dev) return console.log("Could not find the developer user.");
+
+        dev.send({
+            embeds: [
+                Response("BUG REPORTED", args!.join(" "), "WARN")
+                    .setThumbnail(author.displayAvatarURL())
+                    .addField("Sent by", `${author}`, true)
+                    .addField("Guild", `${guild?.name}`, true)
+                    .setTimestamp(Date.now()),
+            ],
+        });
+    },
+};
--- src/commands/admin/index.ts
+++ src/commands/admin/index.ts
@@ -2,3 +2,4 @@ export * from "./ban";
 export * from "./clear";
 export * from "./kick";
 export * from "./renameserver";
+export * from "./bug";
