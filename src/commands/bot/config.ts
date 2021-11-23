0554674f28d86a815c11748c49635ba95831d626
fobdev <silvapedro55@gmail.com>
2021-11-23 10:44:44 -0300

    added config command

---
+++ src/commands/bot/config.ts
@@ -0,0 +1,135 @@
+import { Prefix } from ".";
+import { getGlobalResponse, getLocale, getPrefix, setGlobalResponse, setLocale } from "../../db";
+import { Command } from "../../interfaces";
+import { Response } from "../../models";
+
+export const Config: Command = {
+    name: ["config"],
+    arguments: ["?", "setting] [value"],
+    description:
+        "Configure the bot in the current guild. Use the command without arguments to see the available configurations.\nThis command is only available for admins.",
+    run: async (prefix, client, message, args, player, postgres) => {
+        const { channel, guild } = message;
+
+        if (args?.length === 0)
+            return channel.send({
+                embeds: [
+                    Response(
+                        "List of available configurations",
+                        `Use **${prefix}config [setting] [value]** to change the config in the current server.\nOnly admins can use this command.`,
+                        "SUCCESS"
+                    )
+                        .addField(
+                            ":small_blue_diamond: language",
+                            `Sets the language of the bot\n**Values: EN | BR\nCurrent: ${
+                                (await getLocale(postgres!, guild!)) === "br"
+                                    ? "Português"
+                                    : "English"
+                            }**`,
+                            true
+                        )
+                        .addField(
+                            ":small_blue_diamond: response",
+                            `Sets if the bot will respond when someone says anything that contains 'bog' in a text channel.\nNote: The bot will only respond in Brazilian Portuguese.\n**Values: true | false\nCurrent: ${await getGlobalResponse(
+                                postgres!,
+                                guild!
+                            )}**`,
+                            true
+                        )
+                        .addField(
+                            ":small_blue_diamond: prefix",
+                            `Changes the prefix that the bot will respond to.\n**Values: [new prefix]\nCurrent: ${await getPrefix(
+                                postgres!,
+                                guild!
+                            )}**`,
+                            true
+                        ),
+                ],
+            });
+
+        switch (args![0]) {
+            case "language" || "lang": {
+                switch (args![1]) {
+                    case "en" || "EN" || "english" || "ingles" || "inglês":
+                        if ((await getLocale(postgres!, guild!)) !== "en")
+                            return await setLocale(postgres!, guild!, "en").then(() => {
+                                channel.send("Bot language set to **English**.");
+                            });
+                        return channel.send(
+                            "The bot is already set to respond in English, please select another language."
+                        );
+                    case "br" ||
+                        "BR" ||
+                        "pt" ||
+                        "ptbr" ||
+                        "portugues" ||
+                        "português" ||
+                        "portuguese":
+                        if ((await getLocale(postgres!, guild!)) !== "br")
+                            return await setLocale(postgres!, guild!, "br").then(() => {
+                                channel.send(
+                                    "O idioma Português ainda está em desenvolvimento\nPorém quando os comandos forem traduzidos, o bot responderá em **Português**"
+                                );
+                            });
+                        return channel.send(
+                            "O bot já está selecionado para responder em Português, selecione outro idioma."
+                        );
+                    default:
+                        return channel.send({
+                            embeds: [
+                                Response(
+                                    "Error",
+                                    "Please input a valid language.\nThe available languages are: **EN | BR**",
+                                    "FAIL"
+                                ),
+                            ],
+                        });
+                }
+            }
+            case "response" || "res": {
+                switch (args![1]) {
+                    case "true": {
+                        if ((await getGlobalResponse(postgres!, guild!)) !== true)
+                            return await setGlobalResponse(postgres!, guild!, true).then(() => {
+                                channel.send(
+                                    `The bot will now respond when someone says 'bog' in a text channel.`
+                                );
+                            });
+
+                        return channel.send(`The bot is already responding to 'bog'.`);
+                    }
+                    case "false": {
+                        if ((await getGlobalResponse(postgres!, guild!)) !== false)
+                            return await setGlobalResponse(postgres!, guild!, false).then(() => {
+                                channel.send(`The bot is no longer responding to 'bog'.`);
+                            });
+                        return channel.send(`The bot already is not responding to 'bog'`);
+                    }
+                    default:
+                        return channel.send({
+                            embeds: [
+                                Response(
+                                    "Error",
+                                    "Please input a valid argument.\nThe available arguments are: **true | false**",
+                                    "FAIL"
+                                ),
+                            ],
+                        });
+                }
+            }
+            case "prefix": {
+                return Prefix.run(prefix, client, message, Array(args![1]), player, postgres);
+            }
+            default:
+                return channel.send({
+                    embeds: [
+                        Response(
+                            "Error",
+                            `Please input a valid configuration from the list.\nYou can check the list by using ${prefix}config`,
+                            "FAIL"
+                        ),
+                    ],
+                });
+        }
+    },
+};
--- src/commands/bot/index.ts
+++ src/commands/bot/index.ts
@@ -2,3 +2,4 @@ export * from "./help";
 export * from "./invite";
 export * from "./ping";
 export * from "./prefix";
+export * from "./config";
